485a9e129498a286fecf06c12c0c9dc4e3cc9dc4
PzGlevi <pzglevitpb46@gmail.com>
2024-02-10 18:12:40 -0300

    novo diretorio de image

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,9 @@ function toogleMode() {
     html.classList.toggle("light")
     const image = document.querySelector("#profile img")
     if(html.classList.contains("light")){
-        image.setAttribute('src', "/nui/image/levi_sem_fundo_com_borda_preta.png")
+        image.setAttribute('src', "/assets/image/levi_sem_fundo_com_borda_preta.png")
     } else {
-        image.setAttribute('src', "/nui/image/Levi_sem_fundo.png")
+        image.setAttribute('src', "/assets/image/Levi_sem_fundo.png")
     }
 
     // if(html.classList.contains("light")){
